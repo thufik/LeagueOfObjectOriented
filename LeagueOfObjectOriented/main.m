1c0f4dd84d240f372ca3dcbaf4bf7c75827cd8c9
Vinicius Miana <vinicius@miana.com.br>
2013-12-08 10:53:37 -0200

    Initial Commit

---
+++ LeagueOfObjectOriented/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LeagueOfObjectOriented
+//
+//  Created by Vinicius Miana on 12/8/13.
+//  Copyright (c) 2013 Vinicius Miana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
