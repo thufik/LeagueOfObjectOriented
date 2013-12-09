465fd4a2b4ba55a815fbb1cae24721832fa8a122
ISABELA MAYUMI <31209467@095819-CRT.local>
2013-12-09 17:20:55 -0200

    nslog vazio

--- LeagueOfObjectOriented/main.m
+++ LeagueOfObjectOriented/main.m
@@ -16,7 +16,7 @@ int main(int argc, const char * argv[])
         // insert code here...
      //   NSLog(@"Hello, World!");
         NSLog(@"Passei por aqui");
-        
+        NSLog(@" ");
         
         
         
